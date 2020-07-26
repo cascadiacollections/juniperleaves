4b9728519defc32a5925cdf7f230ff57b83ef4ed
Kevin T. Coughlin <kevin@cascadiacollections.com>
2020-07-26 14:14:20 -0700

    Update slideshow

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-const LISTING_IMAGE_URLS: string[] = [
-    "https://www.etsy.com/listing/734207484/matching-mountain-wonderland-bandana?ref=shop_home_active_3&frs=1",
-    "https://www.etsy.com/listing/748068301/matching-galaxy-bandana-drool-bib-and?ref=shop_home_active_4&frs=1"
-];
-
-export { LISTING_IMAGE_URLS }
